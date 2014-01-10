da22803e077b56ec2843a8425f33813f7726fb76
Nick Willever <nickwillever@gmail.com>
2014-01-09 21:46:54 -0800

    dev updates

--- lib/fenton/auth.rb
+++ lib/fenton/auth.rb
@@ -1,6 +1,6 @@
 module Fenton
   class Auth
-    attr_accessor :username, :email, :private_key, :public_key, :private_key_encoded, :public_key_encoded, :public_key_sha, :api_public_key, :api_public_key_encoded
+    attr_accessor :username, :email, :private_key, :public_key, :private_key_encoded, :public_key_encoded, :public_key_sha, :api_public_key, :api_public_key_encoded, :fenton_server_url
 
     def initialize(attributes=nil)
       if attributes != nil
@@ -15,6 +15,8 @@ module Fenton
         @public_key = Key.decode(@public_key_encoded)
         @api_public_key = Key.decode(@api_public_key_encoded)
         @public_key_sha = Key.sha(@public_key)
+
+        @fenton_server_url = 'http://localhost:9292'
 
         return false if attributes[:auth_public_key_sha] != @public_key_sha
       end
@@ -39,14 +41,47 @@ module Fenton
     def save(file)
       config_contents = File.exists?(file) ? YAML.load_file(file) : {}
 
-      config_contents.store(:username,@username) unless @username.nil?
-      config_contents.store(:email,@email) unless @email.nil?
       config_contents.store(:auth_private_key_encoded,@private_key_encoded)
       config_contents.store(:auth_public_key_encoded,@public_key_encoded)
       config_contents.store(:auth_public_key_sha,@public_key_sha)
+
+      config_contents.store(:username,@username) unless @username.nil?
+      config_contents.store(:email,@email) unless @email.nil?
       config_contents.store(:api_public_key_encoded,@api_public_key_encoded) unless @api_public_key_encoded.nil?
+      config_contents.store(:fenton_server_url,@fenton_server_url) unless @fenton_server_url.nil?
 
       File.open(file, 'w') { |f| f.write(config_contents.to_yaml) }
+    end
+
+    def send_encoded_message(username,message)
+      message_key_encoded, message_encoded = encode_message(message)
+
+      return send(username,message_key_encoded,message_encoded)
+    end
+
+    def encode_message(message)
+      box = RbNaCl::Box.new(@api_public_key, @private_key)
+      nonce = RbNaCl::Random.random_bytes(box.nonce_bytes)
+      nonce_encoded = Key.encode(nonce)
+
+      message_encoded = Key.encode(message.to_s)
+
+      ciphertext = box.encrypt(nonce, message_encoded)
+      ciphertext_encoded = Key.encode(ciphertext)
+
+      return nonce_encoded, ciphertext_encoded
+    end
+
+    def send(username,message_key_encoded,message_encoded)
+      puts @fenton_server_url
+
+      result = Excon.post("#{@fenton_server_url}/users/update", 
+                           :body => URI.encode_www_form(:username => username,
+                                                        :message_key_encoded => message_key_encoded,
+                                                        :message_encoded => message_encoded
+                                                       ),
+                           :headers => { "Content-Type" => "application/x-www-form-urlencoded" })
+      return result
     end
   end
 end
--- lib/fenton/version.rb
+++ lib/fenton/version.rb
@@ -1,3 +1,3 @@
 module Fenton
-  VERSION = "0.0.2"
+  VERSION = "0.0.3"
 end
